381f69cbf4f24b6cce0008ebb4cefca560b6a5c8
QuintyHH <razmocanu@gmail.com>
2021-02-08 07:22:34 +0000

    add and set up UT environment

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import { useEffect } from 'react'
 import { useCustomSelector } from '../store/ContextProvider'
 import { MarsMap } from '../components/MarsMap'
 import { SideMenu } from '../components/SideMenu'
